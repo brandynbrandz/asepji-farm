0513b9a6c77c1feb2b5fcfa3313b671ecba4359b
unknown <brandynodoyo@gmail.com>
2020-11-10 06:22:58 +0300

    remove usestate from header icon

--- frontend/src/components/header/IconGroup.js
+++ frontend/src/components/header/IconGroup.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import MenuCart from "./sub-components/MenuCart";
